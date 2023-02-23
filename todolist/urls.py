4950c2ed8c22fd5b4b1e4523da94ff5bc8bc96bd
Gysborn <forparty@gmail.com>
2023-02-23 21:09:16 +0300

    HomeWork 35

--- todolist/urls.py
+++ todolist/urls.py
@@ -20,6 +20,7 @@ from todolist import settings
 urlpatterns = [
     path('core/', include(('core.urls', 'core'))),
     path('admin/', admin.site.urls),
+    path('oauth/', include('social_django.urls', namespace='social'))
 ]
 
 if settings.DEBUG:
